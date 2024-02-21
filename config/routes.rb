ee7aee224fe62b59bfa363c0e5499df0a8f36cd3
marusky <adrianmarusakza@gmail.com>
2024-02-21 15:55:35 +0100

    Add index and show for notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
-  get 'notes/show', to: 'notes#show'
+
+
+  get 'notes', to: 'notes#index'
+  get 'notes/:id', to: 'notes#show'
 
   root 'pages#home'
 end
