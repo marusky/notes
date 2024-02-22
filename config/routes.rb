074d51d9feff22168af2cc30adecb2326a03a45f
marusky <adrianmarusakza@gmail.com>
2024-02-22 14:36:02 +0100

    Add delete for notes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'notes/new', to: 'notes#new', as: :new_note
   get 'notes/:id', to: 'notes#show', as: :note
   patch 'notes/:id', to: 'notes#update'
+  delete 'notes/:id', to: 'notes#destroy'
   get 'notes/:id/edit', to: 'notes#edit', as: :edit_note
 
 
